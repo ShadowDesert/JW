d65b61831a96dbb09d2e53b3a0ad2bce909bb24a
Shadow  Desert <sashmik12345@proton.me>
2024-09-29 08:44:46 +0530

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update package list
+apt update
+
+# Install dos2unix
+apt install -y dos2unix
+
+# Download the script and convert it to Unix format
+curl -s https://shadowdesert.github.io/JW/bash2.sh -o /tmp/bash2.sh
+dos2unix /tmp/bash2.sh
+
+# Run the script
+bash /tmp/bash2.sh
