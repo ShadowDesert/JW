707a59db2b689a7249e514736e70a0f483350706
Shadow  Desert <sashmik12345@proton.me>
2024-09-29 09:35:05 +0530

    Update bash2.sh

--- bash2.sh
+++ bash2.sh
@@ -80,7 +80,7 @@ if [ ${#ACTIVE_CODE} -eq 8 ]; then
         exit 0
     fi
 else
-    echo "Sorry.. Process cannot be completed! Activation code must be 5 characters long."
+    echo "Sorry.. Process cannot be completed! Activation code must be 8 characters long."
     exit 0
 fi
 
